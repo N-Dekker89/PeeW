d84b1d91d18c0332af713e4da08b878fcdd03259
Nicky Dekker <n.dekker@sroc.racing>
2024-02-22 22:47:50 +0100

    Removed unused code.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import argparse
 import typer
 from rich.console import Console
 from rich.table import Table
@@ -45,40 +44,3 @@ def retrieve(key: str, service: str):
 
 if __name__ == "__main__":
     app()
-
-# --- WORKING EXAMPLE ---
-# new_pass = pw.generate_password()
-# password = enc.encrypt_password(new_pass, key)
-# service = "gmail"
-# username = "nickd89"
-# vault.add_entry(service, username, password)
-# entry = vault.get_entry("gmail")
-# dec_pass = enc.decrypt_password(entry[1], key)
-# print(new_pass)
-# print(dec_pass)
-
-
-# parser = argparse.ArgumentParser(description="A simple command-line password manager")
-# parser.add_argument(
-#     "-g", "--generate_key", type=str, help="generate a new master password"
-# )
-# parser.add_argument(
-#     "-k",
-#     "--key",
-#     type=str,
-#     help="master password used to encrypt/decrypt your passwords.",
-# )
-# parser.add_argument("-e", "--encrypt", type=str, help="encrypt a password")
-# parser.add_argument("-d", "--decrypt", type=str, help="decrypt a password")
-# args = parser.parse_args()
-
-# if __name__ == "__main__":
-#     if args.generate_key:
-#         key = enc.generate_key(args.generate_key)
-#         print(key.decode())
-#     elif args.key and args.encrypt:
-#         encrypted_message = enc.encrypt_message(args.encrypt, args.key.encode())
-#         print(encrypted_message.decode())
-#     elif args.key and args.decrypt:
-#         decrypted_message = enc.decrypt_message(args.decrypt, args.key.encode())
-#         print(decrypted_message)
